a4e4a66f8367f1799d3246400698e34d3a5f48cb
Elizabeth Ruiz Peralta PERALTA <u10716099128@acad.pp.ifms.edu.br>
2026-05-07 12:13:31 -0400

    pacote bean com erpusuario.

---
+++ src/bean/ErpUsuarios.java
@@ -0,0 +1,134 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author u10716099128
+ */
+public class ErpUsuarios {
+    private int erpIdUsuarios;
+    private String erpNome;
+    private String erpApelido;
+    private String erpCpf;
+    private Date erpDataNascimento;
+    private String erpSenha;
+    private int erpNivel;
+    private String erpAtivo;
+
+    /**
+     * @return the erpIdUsuarios
+     */
+    public int getErpIdUsuarios() {
+        return erpIdUsuarios;
+    }
+
+    /**
+     * @param erpIdUsuarios the erpIdUsuarios to set
+     */
+    public void setErpIdUsuarios(int erpIdUsuarios) {
+        this.erpIdUsuarios = erpIdUsuarios;
+    }
+
+    /**
+     * @return the erpNome
+     */
+    public String getErpNome() {
+        return erpNome;
+    }
+
+    /**
+     * @param erpNome the erpNome to set
+     */
+    public void setErpNome(String erpNome) {
+        this.erpNome = erpNome;
+    }
+
+    /**
+     * @return the erpApelido
+     */
+    public String getErpApelido() {
+        return erpApelido;
+    }
+
+    /**
+     * @param erpApelido the erpApelido to set
+     */
+    public void setErpApelido(String erpApelido) {
+        this.erpApelido = erpApelido;
+    }
+
+    /**
+     * @return the erpCpf
+     */
+    public String getErpCpf() {
+        return erpCpf;
+    }
+
+    /**
+     * @param erpCpf the erpCpf to set
+     */
+    public void setErpCpf(String erpCpf) {
+        this.erpCpf = erpCpf;
+    }
+
+    /**
+     * @return the erpDataNascimento
+     */
+    public Date getErpDataNascimento() {
+        return erpDataNascimento;
+    }
+
+    /**
+     * @param erpDataNascimento
+     */
+    public void setErpDataNascimento(Date erpDataNascimento) {
+        this.erpDataNascimento = erpDataNascimento;
+    }
+
+    /**
+     * @return the erpSenha
+     */
+    public String getErpSenha() {
+        return erpSenha;
+    }
+
+    /**
+     * @param erpSenha the erpSenha to set
+     */
+    public void setErpSenha(String erpSenha) {
+        this.erpSenha = erpSenha;
+    }
+
+    /**
+     * @return the erpNivel
+     */
+    public int getErpNivel() {
+        return erpNivel;
+    }
+
+    /**
+     * @param erpNivel the erpNivel to set
+     */
+    public void setErpNivel(int erpNivel) {
+        this.erpNivel = erpNivel;
+    }
+
+    /**
+     * @return the erpAtivo
+     */
+    public String getErpAtivo() {
+        return erpAtivo;
+    }
+
+    /**
+     * @param erpAtivo the erpAtivo to set
+     */
+    public void setErpAtivo(String erpAtivo) {
+        this.erpAtivo = erpAtivo;
+    }
+}
